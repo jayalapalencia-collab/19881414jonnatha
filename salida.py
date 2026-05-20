e3e63d96d1a43eee5314889f5c8851ba2c2fc3ad
Yisus <jayalapalencia@gmail.com>
2026-05-20 16:36:41 -0600

    Add files via upload

---
+++ salida.py
@@ -0,0 +1,16 @@
+x, y ,z = "Python", "es" ,"laCabra"
+print(x, y, z)
+print("")
+x = "Python "
+y = "es "
+z = "laCabra"
+print(x + y + z)
+print("")
+x = 67
+y = 2
+print(x + y)
+print(x , y)
+print("")
+x = 67
+y = "yisus"
+print(x, y)
