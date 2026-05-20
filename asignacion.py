a8348db5b967164046bf8140947d2d8e6154f72e
Yisus <jayalapalencia@gmail.com>
2026-05-20 16:20:09 -0600

    Add files via upload

---
+++ asignacion.py
@@ -0,0 +1,16 @@
+x,y,z = "tamamaz", "tamaguey", "tamatrans"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "tamagay"
+print(x)
+print(y)
+print(z)
+print("")
+fases = ["tamagay", "tamatrans", "tamabi", "tamapiola"]
+x,y,z, a = fases
+print(x)
+print(y)
+print(z)
+print(a)
